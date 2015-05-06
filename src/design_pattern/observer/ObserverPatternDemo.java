ff5e743723a5d3780d87ae7290d119771b24f553
N.Liu <nian.liu80s@gmail.com>
2015-05-07 00:02:17 +0200

    design pattern

---
+++ src/design_pattern/observer/ObserverPatternDemo.java
@@ -0,0 +1,24 @@
+package design_pattern.observer;
+
+import java.util.Scanner;
+
+public class ObserverPatternDemo {
+
+	public static void main(String[] args) {
+		Subject mySubject = new Subject();
+		
+		new BinaryObserver(mySubject);
+		new OctalObserver(mySubject);
+		new HexObserver(mySubject);
+		
+		while (true) {
+			System.out.println("Enter new state (Integer)>>");
+			Scanner in = new Scanner(System.in);
+			String myState= in.nextLine();
+			Integer s = Integer.parseInt(myState);
+			mySubject.setState(Integer.parseInt(myState));
+		}
+		
+	}
+
+}
